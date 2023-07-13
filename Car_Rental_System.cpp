79a221a77b679749958513c9d33a416be24f770e
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-07-12 17:24:24 -0700

    Routine to check Account Existence
    Implemented a routine called checkAccountExistence. Return -1 if account does not exist 1 otherwise.

--- Car_Rental_System.cpp
+++ Car_Rental_System.cpp
@@ -47,3 +47,14 @@ bool isValid(std::string& email)
     const std::regex pattern(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
     return std::regex_match(email, pattern);
 }
+
+int checkAccountExistance(std::map<std::string, User> users, std::string email)
+{
+    if (users.find(email) != users.end())
+    {
+        // Email already exist
+        return 1;
+    }
+
+    return -1;
+}
