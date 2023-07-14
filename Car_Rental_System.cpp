60a7a34d7fe67d6d20da08eaca04d9136cf200e0
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-07-14 16:05:28 -0700

    Made changes to my User Class and Car Class
    Made my routines in my User and Car Class safer by making my variables constants and references. Now users cannot make changes to users data.

--- Car_Rental_System.cpp
+++ Car_Rental_System.cpp
@@ -6,38 +6,40 @@
 #include <map>
 #include <regex>
 
-class User 
+class User
 {
     public:
-    User() = default;
-    User(std::string first, std::string last, std::string password, std::string id);
-    std::string getFirstName() { return firstName; }
-    std::string getLastName() { return lastName; }
-    std::string getPassword() { return password; }
-    std::string getAccountNumber() { return AccountID; }
-    void setEmailAddress(std::string email) { emailAddress = email; }
-    void setPassword(std::string password) { this->password = password; }
-
-    std::string toString();
-
+        User() = default;
+        User(const std::string& first, const std::string& last, const std::string& password, const std::string& number);
+        std::string getFirstName() const { return firstName; }
+        std::string getLastName() const { return lastName; }
+        std::string getEmailAddress() const { return emailAddress; }
+        std::string getPassword() const { return password; }
+        void setEmailAddress(const std::string& email) { emailAddress = email; }
+        void setPassword(const std::string& password) { this->password = password; }
+        std::string getAccountNumber() const { return AccountNumber; }
+        
+        std::string toString() const;
+    
     private:
         std::string firstName;
         std::string lastName;
+        std::string emailAddress;
         std::string password;
-        std::string AccountID;
+        std::string AccountNumber;
 };
 
-class Car 
+class Car
 {
     public:
         Car() = default;
-        Car(std::string name, int quantity, double dailyFee);
-        std::string getName() { return name; }
-        std::string getQuantity() { return quantity; }
-        std::string getDailyFee() { return dailyFee; }
+        Car(const std::string& name, int quantity, double dailyFee);
+        std::string getName() const { return name; }
+        int getQuantity() const { return quantity; }
+        double getDailyFee() const { return dailyFee; }
         bool reserve();
-        double calculateDailyFee();
-
+        double calculateRentalCost(int days) const;
+    
     private:
         std::string name;
         int quantity;
